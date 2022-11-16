a9673792092a980a4e56a4dcbb061119a3ca4c28
linat21 <leatheberner@gmail.com>
2022-11-16 19:34:30 +0300

    update: templates refactor: footer, index add: about, contact

--- windstore/windstore/urls.py
+++ windstore/windstore/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index),
     path('shop/', include('shop.urls', namespace='shop')),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('about/', views.about),
+    path('contact/', views.contact),
 ]
--- windstore/windstore/views.py
+++ windstore/windstore/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     template = 'index.html'
     return render(request, template)
+
+
+def about(request):
+    template = 'about.html'
+    return render(request, template)
+
+
+def contact(request):
+    template = 'contact.html'
+    return render(request, template)
