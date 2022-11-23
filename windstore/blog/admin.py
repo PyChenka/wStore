4cfae497beb204b24050c39b88ceb5b49aec0ca9
linat21 <leatheberner@gmail.com>
2022-11-23 15:16:02 +0300

    init: admin add: shop/admin, blog/admin update: "light mode" admin panel

--- windstore/blog/admin.py
+++ windstore/blog/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'show_image',
+        'title',
+        # 'show_products',
+        'time_create',
+        'time_update',
+        'is_published',
+    )
+    list_display_links = ('title', )
+    filter_horizontal = ('products', )
+    search_fields = ('title', )
+    list_filter = ('time_create', )
+    fields = (
+        'show_image',
+        'title',
+        'content',
+        'image',
+        'products',
+    )
+    readonly_fields = ('show_image', )
+
+    # def show_products(self, obj):
+    #     """Отображает ссылки на товары из статьи (ManyToManyField) на главной странице админки"""
+    #     return [prod.images.first().image for prod in obj.products.all()]
+
+    def show_image(self, obj):
+        """Отображает изображения на главной странице админки"""
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width=50>')
+
+    show_image.short_description = 'image'
+
+
+admin.site.register(Article, ArticleAdmin)
--- windstore/shop/admin.py
+++ windstore/shop/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Gallery, Product, Review
+
+
+class GalleryInLine(admin.TabularInline):
+    fk_name = 'product'
+    model = Gallery
+
+
+# @admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'show_image',
+        'title',
+        'price',
+        'slug',
+        'available',
+    )
+    list_display_links = ('title', )
+    search_fields = ('title', )
+    list_filter = ('time_create', 'price', )
+    fields = (
+        'title',
+        'show_image',
+        'slug',
+        'description',
+        'specification',
+        'price',
+        'available',
+    )
+    readonly_fields = ('show_image', )
+    inlines = [GalleryInLine, ]
+
+    def show_image(self, obj):
+        """Отображает главное изображение из Gallery товара"""
+        return mark_safe(f'<img src="{obj.images.first().image.url}" width=50>')
+
+    show_image.short_description = 'image'
+
+
+admin.site.register(Product, ProductAdmin)
+
