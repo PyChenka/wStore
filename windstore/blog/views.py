f2116e3094a44796c5e584379b3fefdabe9d99c7
linat21 <leatheberner@gmail.com>
2022-11-28 23:18:53 +0300

    refactor: blog.views

--- windstore/blog/views.py
+++ windstore/blog/views.py
@@ -1,18 +1,40 @@
-from django.http import HttpResponse, Http404
+from django.http import Http404
 from django.shortcuts import render, redirect
+from django.views.generic import ListView
 
 from blog.models import Article
+from windstore.views import MENU
+
+
+CONTEXT = {
+        'menu': MENU,
+        'name': 'Windstore',
+        'headline': ' - Blog',
+        'type': 'articles',
+        'title': 'Articles.'
+}
+
+
+class ArticlesAll(ListView):
+    model = Article
+    template_name = 'catalog.html'
+    context_object_name = 'objects'
+    extra_context = CONTEXT
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        return Article.objects.all()
 
 
 def show_all_articles(request):
     """Отображает все статьи, упорядоченные от самой новой"""
-    template = 'blog/articles.html'
+    template = 'catalog.html'
     articles = Article.objects.order_by('-time_create')
-    context = {
-        'type': 'articles',
-        'title': 'Articles.',
-        'objects': articles,
-    }
+    context = CONTEXT
+    context['objects'] = articles
     return render(request, template, context)
 
 
@@ -20,9 +42,8 @@ def show_single_article(request, slug):
     """Отображает отдельную статью"""
     template = 'blog/article.html'
     article = Article.objects.get(time_create__date=slug)
-    context = {
-        'article': article,
-    }
+    context = CONTEXT
+    context['article']: article
     return render(request, template, context)
 
 
@@ -33,12 +54,9 @@ def show_articles_by_year(request, year):
     elif int(year) > 2023:
         return redirect('blog:index')
 
-    template = 'blog/articles.html'
+    template = 'catalog.html'
     articles = Article.objects.filter(time_create__year=year).order_by('-time_create')
-    context = {
-        'type': 'articles',
-        'year': year,
-        'title': 'Articles.',
-        'objects': articles,
-    }
+    context = CONTEXT
+    context['year']: year
+    context['objects']: articles
     return render(request, template, context)
